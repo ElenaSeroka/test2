20b76f184f0f23553b4d19cf3b33cacc4a81ffaa
Elena Seroka <pm222hs@student.lnu.se>
2022-08-22 20:42:45 +0200

    cleaned up controller,  made functions for configuring data for chart

--- controllers/page-controller.js
+++ controllers/page-controller.js
@@ -22,34 +22,7 @@ export class PageController {
      * @param {object} res - Express response object.
      */
 
-
-    welcomeMessage(req, res) {
-    }
-
-    async renderMain(req, res) {
-        res.json('rendermain Hello')
-        console.log('rendermain')
-    }
-
-    async getChartShit(req, res) {
-        const data = [
-            {
-                labels: ['Boo', 'February', 'March', 'April', 'May', 'June', 'Spooktober'],
-                data: [{
-                    values: [2, 5, 10, 25, 75, 43, 23],
-                },
-                {
-                    values: [21, 45, 28, 80, 150, 43, 23]
-                }]
-            }
-        ]
-        console.log(data)
-        res.json(data)
-    }
-
-
-    async getAllGames(req, res) {
-        console.log('getAllGames func')
+    async getGamesPerYear(req, res) {
 
         const client = await new Client({
             node: process.env.NODE_URL,
@@ -58,50 +31,28 @@ export class PageController {
                 password: process.env.PASSWORD
             }
         })
-        // The following code adds data to the elastic search. Only run this once!  
-        // await addToDB(client)
 
-        // const searchResults = await client.search({
-        //   'size': 100,
-        //   "query": {
-        //     "match_all": {}
-        //   }
-        // })
+        const defaultSize = 5000
 
-
-
-        // const keyword = 'sonic'
-        // const size = 20
-        const defaultSize = 4000
-        const year = '2004-07'
-        const decade = 20
-
-        //   Let's search!
-        // detta ska in i express appen. conmect. ny connect varje gång
         const searchResults = await client.search({
-            // size: `${size}` || defaultSize,
             size: defaultSize,
             index: 'games',
-
-            // query: {
-            //   match: {
-            //     Firstreleased: '2002-12-12'
-            //   }
-            // }
 
             query: {
                 bool: {
                     must: [
                         {
-                            match: {
-                                JP: 'Unreleased'
-                            }
-                        },
-                        {
+
+                            "multi_match" : {
+                                "query":    "Released", 
+                                "fields": [ "JP", "EUPAL" ] 
+                            },
+
+
                             range: {
                                 Firstreleased: {
-                                    gte: '2002-06-25',
-                                    lte: '2002-06-29'
+                                    gte: '1900-01-01',
+                                    lte: '2020-01-01'
                                 }
                             }
                         }
@@ -110,30 +61,258 @@ export class PageController {
             }
         })
 
-        console.log(searchResults.hits.hits)
+        const allResults = searchResults.hits.hits.map(game => game._source.Firstreleased)
+        const allResultsSorted = allResults.sort()
+        const configuredData = configureYearForChart(allResultsSorted)
+        let result = getLabelsAndData(configuredData)
+        let labels = result[0]
+        let dataPoints = result[1]
 
-        console.log(searchResults.hits.hits.length)
 
-        res.json(searchResults.hits.hits)
 
-        // const data = [
-        //     {
-        //         labels: ['Boo', 'February', 'March', 'April', 'May', 'June', 'July'],
-        //         data: [{
-        //             values: [2, 5, 10, 25, 75, 43, 23],
-        //         },
-        //         {
-        //             values: [21, 45, 28, 80, 99, 43, 23]
-        //         }]
-        //     }
-        // ]
+        const data = [
+            {
+                labels: labels,
+                data: {
+                    values: dataPoints,
+                }
+            }
+        ]
+        res.json(data)
+    }
+
+    async getGamesAllYearsJapanAndEurope(req, res) {
+        const client = await new Client({
+            node: process.env.NODE_URL,
+            auth: {
+                username: process.env.USER_NAME,
+                password: process.env.PASSWORD
+            }
+        })
+
+        const defaultSize = 5000
+
+        const searchResultsJapan = await client.search({
+            size: defaultSize,
+            index: 'games',
+
+            query: {
+                bool: {
+                    must: [
+                        {
+                            match: {
+                                JP: 'Released'
+                            }
+                        },
+                        {
+                            range: {
+                                Firstreleased: {
+                                    gte: '1900-01-01',
+                                    lte: '2020-01-01'
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+        })
+        const searchResultsEUPAL = await client.search({
+            size: defaultSize,
+            index: 'games',
+
+            query: {
+                bool: {
+                    must: [
+                        {
+                            match: {
+                                EUPAL: 'Released'
+                            }
+                        },
+                        {
+                            range: {
+                                Firstreleased: {
+                                    gte: '1900-01-01',
+                                    lte: '2020-01-01'
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+        })
+
+        let resultJP = getLabelsAndDataForYears(searchResultsJapan)
+        let resultEU = getLabelsAndDataForYears(searchResultsEUPAL)
+
+        let labels = resultJP[0]
+
+        let dataPointsJP = resultJP[1]
+        let dataPointsEU = resultEU[1]
+
+        const data = [
+            {
+                labels: labels,
+                data: [{
+                    values: dataPointsJP,
+                }
+                    ,
+                {
+                    values: dataPointsEU
+                }
+                ],
+                dataset: {
+                    country1: 'Japan',
+                    country2: 'EUPAL'
+
+                }
+            }
+        ]
+
+        res.json(data)
+
 
     }
 
+    async getGameCompany(req, res) {
+        const client = await new Client({
+            node: process.env.NODE_URL,
+            auth: {
+                username: process.env.USER_NAME,
+                password: process.env.PASSWORD
+            }
+        })
+
+        const defaultSize = 5000
+
+        const searchResults = await client.search({
+            size: defaultSize,
+            index: 'games',
+
+            query: {
+                bool: {
+                    must: [
+                        // {
+                        //     match: {
+                        //         JP: 'Released'
+                        //     }
+                        // },
+                        {
+                            range: {
+                                Firstreleased: {
+                                    gte: '1900-01-01',
+                                    lte: '2020-01-01'
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+        })
+
+        const allResults = searchResults.hits.hits.map(game => game._source.Publisher)
+        const allResultsSorted = allResults.sort()
+        const configuredData = configureDataForChart(allResultsSorted)
+        let result = getLabelsAndData(configuredData)
+        let labels = result[0]
+        let dataPoints = result[1]
 
 
+        const data = [
+            {
+                labels: labels,
+                data: [{
+                    values: dataPoints,
+                }
+                ]
+            }
+        ]
+        res.json(data)
 
-    //---------------------------------------------------------GET----------------------------------------------------------------------------------
+    }
 
-    
+}
+
+function configureDataForChart(sortedData) {
+    let label = []
+    let count = 0
+    let countAllYears = []
+
+    for (let i = 0; i < sortedData.length; i++) {
+        let previousElement = sortedData[i]
+        let year = previousElement
+        let nextNextYear
+        let lastElement = sortedData.length - 1
+        if (i === lastElement) {
+            nextNextYear = 'different from year'
+        } else {
+            nextNextYear = sortedData[i + 1]
+
+        }
+
+        if (year === nextNextYear) {
+            count++
+        }
+
+        if (year !== nextNextYear) {
+            label.push(year)
+            let countPerYear = {}
+            countPerYear[year] = count
+
+            countAllYears.push(count)
+            count = 0
+        }
+    }
+
+    let result = [label, countAllYears]
+    return result
+}
+
+function configureYearForChart(sortedYears) {
+    let gamesCount = 0
+    let labelyears = []
+    let gamesPerYearCount = []
+    for (let i = 0; i < sortedYears.length; i++) {
+        let previousElement = sortedYears[i].substring(0, 4)
+        let year = previousElement
+        let nextNextYear
+        let lastElement = sortedYears.length - 1
+        if (i === lastElement) {
+            nextNextYear = 'different from year'
+        } else {
+            nextNextYear = sortedYears[i + 1].substring(0, 4)
+
+        }
+
+        if (year === nextNextYear) {
+            gamesCount++
+        }
+
+        if (year !== nextNextYear) {
+            labelyears.push(year)
+            let gamePerYearIndividualYear = {}
+            gamePerYearIndividualYear[year] = gamesCount
+
+            gamesPerYearCount.push(gamesCount)
+            gamesCount = 0
+        }
+    }
+
+    return [labelyears, gamesPerYearCount]
+}
+
+function getLabelsAndDataForYears(searchResults) {
+    let allDates = searchResults.hits.hits.map(game => game._source.Firstreleased)
+    const allDatesSorted = allDates.sort()
+    const configuredData = configureYearForChart(allDatesSorted)
+    let result = getLabelsAndData(configuredData)
+    return result
+}
+
+function getLabelsAndData(configuredData) {
+
+    const labels = configuredData[0]
+    const amount = configuredData[1]
+
+    return [labels, amount]
+
 }
