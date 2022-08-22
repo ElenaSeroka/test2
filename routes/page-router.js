42eeb720fe1fe04e53ab3d9b3a11ac6b3dddb629
Elena Seroka <pm222hs@student.lnu.se>
2022-08-22 20:44:50 +0200

    added routes ofr my 3 endpoints

--- routes/page-router.js
+++ routes/page-router.js
@@ -4,6 +4,9 @@ import {PageController} from '../controllers/page-controller.js'
 export const pageRouter = express.Router()
 export const controller = new PageController()
 
-pageRouter.get('/api', controller.getChartShit)
-pageRouter.get('/api/all-games', controller.getAllGames)
-pageRouter.get('/', controller.renderMain)
+// pageRouter.get('/api/all-games', controller.getAllGames)
+pageRouter.get('/api/games-per-year', controller.getGamesPerYear)
+pageRouter.get('/api/games-all-years-JP-EU', controller.getGamesAllYearsJapanAndEurope)
+pageRouter.get('/api/games-per-publisher', controller.getGameCompany)
+// pageRouter.get('/api/all-released-games-specific-year', controller.getAllGamesForASpecificYear)
+
